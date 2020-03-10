514423c8d3148bc65af118b1cd3b1748ba645b34
Taranpreet Singh <taran99.singh@gmail.com>
2020-03-10 14:27:07 -0400

    Added loading text in storyboard

---
+++ PetAssist/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PetAssist
+//
+//  Created by Taranpreet Singh on 2020-03-10.
+//  Copyright © 2020 Taranpreet Singh Yu Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func unwindToHome(sender : UIStoryboardSegue){
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
